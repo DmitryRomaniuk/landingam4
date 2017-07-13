51fb392f75bbf037f4cacab59e0c6f4c26d6ef88
Dzmitry Ramaniuk <kanumowa@gmail.com>
2017-07-14 01:09:51 +0300

    fixed bug with mongoose connection

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ app.use('/', index);
 app.use('/users', users);
 
 mongoose.Promise = global.Promise;
-const uri = 'mongodb://heroku_6q05xjb0:aeamao92plaq974hja2nbhdhs5@ds145292.mlab.com:45292/heroku_6q05xjb0';
-const mongoConnect = mongoose.createConnection(uri,{
-    useMongoClient: true,
-});
+const databaseUri = 'mongodb://heroku_6q05xjb0:aeamao92plaq974hja2nbhdhs5@ds145292.mlab.com:45292/heroku_6q05xjb0';
+mongoose.connect(databaseUri, { useMongoClient: true })
+      .then(() => console.log(`Database connected at ${databaseUri}`))
+      .catch(err => console.log(`Database connection error: ${err.message}`));
 
 const articles = new Schema({
     title: { type: String },
@@ -44,18 +44,56 @@ const articles = new Schema({
     avatarUrl: { type: String },
     likes: { type: Number },
     comments: { type: String },
-    date: { type: Number },
-    author: { type: String }
+    date: { type: Date },
+    author: { type: String },
 }, { collection: 'articles' });
 
+const games = new Schema({
+    title: { type: String },
+    description: { type: String },
+    imgUrl: { type: String },
+    platforms: { type: Array },
+    likes: { type: Number },
+    comments: { type: String },
+    rating: { type: Number },
+}, { collection: 'games' });
+
+const news = new Schema({
+    title: { type: String },
+    description: { type: String },
+    imgUrl: { type: String },
+}, { collection: 'news' });
+
+const trailers = new Schema({
+    title: { type: String },
+    description: { type: String },
+    imgUrl: { type: String },
+}, { collection: 'trailers' });
+
 const myArticles = mongoose.model('myArticles', articles);
-mongoConnect.then(db => {
-  console.log(`DB connected`);
-  myArticles.find({}, function (err, person) {
-      if (err) return console.log(err);
-      console.log(person) // Space Ghost is a talk show host.
-  });
-});
+const myGames = mongoose.model('myGames', games);
+const myNews = mongoose.model('myNews', news);
+const myTrailers = mongoose.model('myTrailers', trailers);
+myArticles.find({})
+  .then(function (articles) {
+      console.log(articles) // Space Ghost is a talk show host.
+  })
+  .catch(err => console.log(`Error in myArticles model: ${err}`));
+myGames.find({})
+  .then(function (articles) {
+      console.log(articles) // Space Ghost is a talk show host.
+  })
+  .catch(err => console.log(`Error in myArticles model: ${err}`));
+myNews.find({})
+  .then(function (articles) {
+      console.log(articles) // Space Ghost is a talk show host.
+  })
+  .catch(err => console.log(`Error in myArticles model: ${err}`));
+myTrailers.find({})
+  .then(function (articles) {
+      console.log(articles) // Space Ghost is a talk show host.
+  })
+  .catch(err => console.log(`Error in myArticles model: ${err}`));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
